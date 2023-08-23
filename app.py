abb3aa20f7aa7d02ed961125c15ebc3c43394553
Ben Richardson <benrichardson@bens-air.lan>
2023-08-22 19:29:38 -0500

    new url in flask

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from pymongo import MongoClient
 mongo = MongoClient(port=27017)
 db = mongo.chicago_bikes
 
-divvy_rides = db['divvy_ridedata']
+divvy_ridedata_merged = db['divvy_ridedata_merged']
 
 # Create a Flask app
 app = Flask(__name__)
@@ -28,14 +28,42 @@ def welcome():
     return (
         "Available Routes:<br/>"
         "/api/v1.0/stations<br/>"
+        "/api/v1.0/start_stations<br/>"
     )
 
 @app.route("/api/v1.0/stations")
 
 def stations():
     """Return a list of stations from the dataset."""
-    station_list = divvy_rides.distinct("start_station_name")
+    station_list = divvy_ridedata_merged.distinct("start_station_name")
     return jsonify(station_list)
+
+@app.route("/api/v1.0/start_stations")
+
+def start_stations():
+    """Return a top to start stations from the dataset."""
+
+    # Use aggregation pipeline to find top ten start stations 
+    pipeline = [
+        {
+            "$group": {
+                "_id": "$start_station_name",
+                "count": {"$sum": 1},
+                "latitude": {"$first": "$start_lat"},
+                "longitude": {"$first": "$start_lng"}
+            }
+        },
+        {
+            "$sort": {"count": -1}
+        },
+        {
+            "$limit": 11
+        }
+    ]
+
+# Perform the aggregation
+    start_station_result = list(divvy_ridedata_merged.aggregate(pipeline))
+    return jsonify(start_station_result)
 
 if __name__ == "__main__":
     app.run(debug=True)
