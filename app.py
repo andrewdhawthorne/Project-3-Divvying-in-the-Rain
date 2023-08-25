3d367e5055f3ed3db450735c3635e2f97d82c206
andrewdhawthorne <adhawthorne@gmail.com>
2023-08-24 23:12:23 -0500

    troubleshooting api routes

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ db = mongo.chicago_bikes
 
 divvy_ridedata_merged = db['divvy_ridedata_merged']
 weather_daily = db['weather_daily']
-divvy_ridedata_merged = db['divvy_ridedata_merged']
 withoutStation = db['withoutStationName']
 withStation = db['withStationName']
 withLatLong = db['withLatLong']
@@ -44,6 +43,13 @@ def welcome():
         "/api/v1.0/start_stations<br/>"
         "/api/v1.0/end_stations<br/>"
         "/api/v1.0/top_routes<br/>"
+        "/api/v1.0/daily_weather<br/>"
+        "/api/v1.0/without_station<br/>"
+        "/api/v1.0/with_station<br/>"
+        "/api/v1.0/with_lat_long<br/>"
+        "/api/v1.0/route_distance<br/>"
+        "/api/v1.0/rides_by_month<br/>"
+        "/api/v1.0/rides_by_season<br/>"
     )
 
 @app.route("/api/v1.0/stations")
@@ -56,23 +62,90 @@ def stations():
 @app.route("/api/v1.0/start_stations")
 
 def start_stations():
-    """Return a top to start stations from the dataset."""
+    """Return top ten start stations from the dataset."""
     Top10StartStations = db['Top10StartStations'].find()
     return jsonify(list(Top10StartStations))
 
 @app.route("/api/v1.0/end_stations")
 
 def end_stations():
-    """Return a top to start stations from the dataset."""
+    """Return top ten end stations from the dataset."""
     Top10EndStations = db['Top10EndStations'].find()
     return jsonify(list(Top10EndStations))
 
 @app.route("/api/v1.0/top_routes")
 
 def top_routes():
-    """Return a top to start stations from the dataset."""
-    Top10EndStations = db['Top10EndStations'].find()
-    return jsonify(list(Top10EndStations))
+    """Return a list of top ten routes from the dataset."""
+    Top10Routes = db['Top10Routes'].find()
+    return jsonify(list(Top10Routes))
+
+@app.route("/api/v1.0/daily_weather")
+
+def daily_weather():
+    """Return daily_weather dataset."""
+    weather_daily = db['weather_daily'].find()
+
+     # Convert ObjectId to string in each document
+    weather_daily = [
+        {**weather, '_id': str(weather['_id'])} for weather in weather_daily
+    ]
+
+    return jsonify(list(weather_daily))
+
+@app.route("/api/v1.0/without_station")
+
+def without_station():
+    """Return list of rides without start and/or end stations."""
+    withoutStation = db['withoutStationName'].find()
+
+     # Convert ObjectId to string in each document
+    withoutStation = [
+        {**ride, '_id': str(ride['_id'])} for ride in withoutStation
+    ]
+
+    return jsonify(list(withoutStation))
+
+@app.route("/api/v1.0/with_station")
+
+def with_station():
+    """Return list of rides with start and/or end stations."""
+    withStation = db['withStationName'].find()
+
+    # Convert ObjectId to string in each document
+    withStation = [
+        {**ride, '_id': str(ride['_id'])} for ride in withStation
+    ]
+
+    return jsonify(list(withStation))
+
+@app.route("/api/v1.0/with_lat_long")
+
+def with_lat_long():
+    """Return list of rides with latitude and longitude."""
+    withLatLong = db['withLatLong'].find()
+    return jsonify(list(withLatLong))
+
+@app.route("/api/v1.0/route_distance")
+
+def route_distance():
+    """Return list of rides with distance between start and end points."""
+    RouteDistance = db['RouteDistance'].find()
+    return jsonify(list(RouteDistance))
+
+@app.route("/api/v1.0/rides_by_month")
+
+def rides_by_month():
+    """Return list of rides by month."""
+    divvy_rides_by_month = db["divvy_rides_by_month"].find()
+    return jsonify(list(divvy_rides_by_month))
+
+@app.route("/api/v1.0/rides_by_season")
+
+def rides_by_season():
+    """Return list of rides by season."""
+    divvy_rides_by_season = db["divvy_rides_by_season"].find()
+    return jsonify(list(divvy_rides_by_season))
 
 if __name__ == "__main__":
     app.run(debug=True)
